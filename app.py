06779b3a8229a84af2f960ac1bf7afc9b290ec4a
Jesse <83730324+dev-jesse@users.noreply.github.com>
2022-10-16 08:33:18 -0400

    fixed indices + added comments

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def get_response():
     examples=[Example("give me hope", "motivation"), Example("show me some motivation", "motivation"), Example("amp my day up", "motivation"), Example("get me hyped up for the day", "motivation"), Example("say something inspirational", "motivation"), Example("why am i always so tired", "stress"), Example("how to calm down", "stress"), Example("my brain hurts", "stress"), Example("why am i so mad", "stress"), Example("frustrated with everything", "stress"), Example("i am constantly sad", "anxiety"), Example("why do i always overthink", "anxiety"), Example("i get so nervous", "anxiety"), Example("worrying all the time", "anxiety"), Example("mind wont shut off", "anxiety"), Example("tell me a joke", "laugh"), Example("say something funny", "laugh"), Example("give me something to laugh to", "laugh"), Example("help cheer me up", "laugh"), Example("make my day better", "laugh")]) 
     
     replies_list = replies[response.classifications[0].prediction]
-    res = replies_list[randint(1, len(replies_list) - 1)]
+    res = replies_list[randint(0, len(replies_list) - 1)]  # the quote we wat
     
     request.args.update
     
